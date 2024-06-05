5a7603f375d378bebb483f87d38e716c8d850f86
Marcio Bueno <marciobueno@gmail.com>
2024-06-05 22:02:48 +0000

    incluído o elemento IMG

--- ex10/script.js
+++ ex10/script.js
@@ -1,11 +1,16 @@
 const btCarregar = document.getElementById("btCarregar");
 const preDados = document.getElementById("preDados");
+const divImg = document.getElementById("divImg");
 
 const handleBtCarregarClick = async () => {
   const response = await fetch("https://random.dog/woof.json");
   const data = await response.json();
   console.log('data', data);
   preDados.innerHTML = JSON.stringify(data);
+
+  divImg.innerHTML = "";
+  const img = document.createElement("img");
+  img.src = data.url;
 };
 
 btCarregar.onclick = handleBtCarregarClick;
