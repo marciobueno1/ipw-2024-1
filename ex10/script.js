84fef2a69f5964df2fb7db52bb4b17cf69cb02dd
Marcio Bueno <marciobueno@gmail.com>
2024-06-05 21:56:04 +0000

    exemplo fetch

---
+++ ex10/script.js
@@ -0,0 +1,11 @@
+const btCarregar = document.getElementById("btCarregar");
+const preDados = document.getElementById("preDados");
+
+const handleBtCarregarClick = async () => {
+  const response = await fetch("https://random.dog/woof.json");
+  const data = await response.json();
+  console.log('data', data);
+  preDados.innerHTML = JSON.stringify(data);
+};
+
+btSubst.onclick = handleBtCarregarClick;
